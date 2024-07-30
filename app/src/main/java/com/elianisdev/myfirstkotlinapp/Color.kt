dee2dc06412de2099a73d67c9fdf416f4befac0e
elianismedina <elianismedina05@outlook.com>
2024-07-30 06:48:58 -0500

    finished login screen

---
+++ app/src/main/java/com/elianisdev/myfirstkotlinapp/Color.kt
@@ -0,0 +1,7 @@
+package com.elianisdev.myfirstkotlinapp
+
+import androidx.compose.ui.graphics.Color
+
+val color1 = Color(0xFF06b6d4)
+val color2 = Color(0xFF6366f1)
+val color3 = Color(0xFFf8fafc)
--- app/src/main/java/com/elianisdev/myfirstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/elianisdev/myfirstkotlinapp/MainActivity.kt
@@ -1,101 +1,121 @@
 package com.elianisdev.myfirstkotlinapp
 
 import android.os.Bundle
-
-
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.elianisdev.myfirstkotlinapp.ui.theme.MyFirstKotlinAppTheme
+
+//Main activity class
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
         //defines the activity's layout where composable functions are called
         setContent {
-            MyFirstKotlinAppTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(Message("Android", "Jetpack Compose"))
-                }
-
-            }
+            LoginScreen()
         }
     }
 }
-data class Message(val author: String, val body: String)
-
 @Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-
+fun LoginScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 30.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Image(
-            painter = painterResource(id = R.drawable.profile_picture),
-            contentDescription = null,
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(
-                    1.5.dp,
-                    MaterialTheme.colorScheme.primary,
-                    CircleShape
-                )
+            painter = painterResource(id = R.drawable.planet),
+            contentDescription = "Login Image",
+            modifier = Modifier.size(200.dp)
         )
-
-        Spacer(modifier = Modifier.width(8.dp))
-
-        Column {
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colorScheme.secondary,
-                style = MaterialTheme.typography.titleSmall
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp ) {
-                Text(
-                    text = msg.body,
-                    style = MaterialTheme.typography.bodyMedium
+        Text(
+            text = "Bienvenido!",
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.headlineLarge
+        )
+        Spacer(modifier = Modifier.size(12.dp))
+        Text(text = "Inicia sesión en tu cuenta")
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "Correo electrónico") })
+        Spacer(modifier = Modifier.size(6.dp))
+        OutlinedTextField(value = "", onValueChange = {}, label = { Text(text = "Contraseña") })
+        Spacer(modifier = Modifier.size(12.dp))
+        GradientButton(
+            text = "Iniciar sesión",
+            textColor = Color.White,
+            gradient = Brush.horizontalGradient(
+                colors = listOf(
+                    color1,
+                    color2
                 )
-            }
+            )
+        ) { }
+        Spacer(modifier = Modifier.size(6.dp))
+        Text(text = "¿Olvidaste tu contraseña?", modifier = Modifier.clickable {
+            //Forgot password
+        })
+        Spacer(modifier = Modifier.size(32.dp))
+        Text(text = "O inicia sesión con")
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxSize().padding(40.dp)
+        ) {
+            Image(painter = painterResource(id = R.drawable.facebook_color_svgrepo_com),
+                contentDescription = "Facebook",
+                modifier = Modifier.size(60.dp).clickable {
+                    //Facebook login
+                })
+            Image(painter = painterResource(id = R.drawable.google_color_svgrepo_com),
+                contentDescription = "Google",
+                modifier = Modifier.size(60.dp).clickable {
+                    //Google login
+                })
+            Image(painter = painterResource(id = R.drawable.twitter_color_svgrepo_com),
+                contentDescription = "Twitter",
+                modifier = Modifier.size(60.dp).clickable {
+                    //Twitter login
+                })
+
         }
 
     }
 
+    }
 
-}
 
-@Preview()
 
-//We cannot preview the composable function directly. We need to wrap it in a composable function that applies the theme
+
+//Preview function to display the UI
+@Preview( showBackground = true)
 @Composable
-fun PreviewMessageCard() {
-    MyFirstKotlinAppTheme {
-        Surface {
-            MessageCard(Message("Lexi", "Jetpack Compose, it is great!"))
-        }
+fun LoginPreview(){
+    LoginScreen()
 
-    }
 }
